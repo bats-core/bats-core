857bcf4649c3cbb166eafe5801cf0c3282af4e53
Martin Schulze <martin.schulze@vireso.de>
2020-08-21 23:10:05 +0200

    Use fixed slot names for semaphores
    this avoids dealing with pids

--- lib/bats-core/semaphore.bash
+++ lib/bats-core/semaphore.bash
@@ -7,8 +7,10 @@
 # when there is a free slot, run the command in background
 # gather the output of the command in files in the given directory
 bats_semaphore_run() {
-    bats_semaphore_acquire_slot
-    bats_semaphore_release_wrapper "$@" >/dev/null 2>/dev/null &
+    local output_dir=$1
+    shift
+    local semaphore_slot=$(bats_semaphore_acquire_slot)
+    bats_semaphore_release_wrapper "$output_dir" "$semaphore_slot" "$@" &
     printf "%d\n" "$!"
 }
 
@@ -18,33 +20,45 @@ export BATS_SEMAPHORE_DIR="$BATS_RUN_TMPDIR/semaphores"
 # $@ - command to run
 # this wraps the actual function call to install some traps on exiting
 bats_semaphore_release_wrapper() {
-    # this background subprocess takes over the semaphore from its parent
-    # on unix mv uses rename, which is atomic on the same filesystem
-    # iff mv is atomic, we don't need to lock, as there is no observable
-    # state where this operation changes the number of used semaphore slots
-    # as we are a special (background) subshell $$ is our parent's pid and $BASHPID is our own
-    mv "$BATS_SEMAPHORE_DIR/pid-$$" "$BATS_SEMAPHORE_DIR/pid-$BASHPID"
-    trap bats_semaphore_release_slot EXIT
+    local output_dir="$1"
+    local semaphore_name="$2"
+    shift 2 # all other parameters will be use for the command to execute
 
-    output_dir="$1"
-    shift # all other parameters will be use for the command to execute
+    trap "bats_semaphore_release_slot '$semaphore_name'" EXIT
 
     mkdir -p "$output_dir"
     "$@" 2>"$output_dir/stderr" >"$output_dir/stdout"
 
     # bash bug: the exit trap is not called for the background process
-    bats_semaphore_release_slot
+    bats_semaphore_release_slot "$semaphore_name"
     trap '' EXIT # avoid calling release twice
 }
 
+bats_semaphore_acquire_while_locked() {
+    if [[ $(bats_semaphore_get_free_slot_count) -gt 0 ]]; then
+        local slot=0
+        while [[ -e "$BATS_SEMAPHORE_DIR/slot-$slot" ]]; do
+            (( ++slot ))
+        done
+        if [[ $slot -lt $BATS_SEMAPHORE_NUMBER_OF_SLOTS ]]; then
+            touch "$BATS_SEMAPHORE_DIR/slot-$slot" && printf "$slot\n" && return 0
+        fi
+    fi
+    return 1
+}
+
+export -f bats_semaphore_acquire_while_locked
+
 # block until a semaphore slot becomes free
+# prints the number of the slot that it received
 bats_semaphore_acquire_slot() {
     mkdir -p "$BATS_SEMAPHORE_DIR"
     # wait for a slot to become free
+    # TODO: avoid busy waiting by using signals -> this opens op prioritizing possibilities as well
     while true; do
         # don't lock for reading, we are fine with spuriously getting no free slot
         if [[ $(bats_semaphore_get_free_slot_count) -gt 0 ]]; then
-            flock "$BATS_SEMAPHORE_DIR" bash -c "[[ \$(bats_semaphore_get_free_slot_count) -gt 0 ]] && touch '$BATS_SEMAPHORE_DIR/pid-$$'" && break
+            flock "$BATS_SEMAPHORE_DIR" bash -c bats_semaphore_acquire_while_locked && break
         fi
         sleep 1
     done
@@ -53,13 +67,13 @@ bats_semaphore_acquire_slot() {
 bats_semaphore_release_slot() {
     # we don't need to lock this, since only our process owns this file
     # and freeing a semaphore cannot lead to conflicts with others
-    rm "$BATS_SEMAPHORE_DIR/pid-$BASHPID" # this will fail if we had not aqcuired a semaphore!
+    rm "$BATS_SEMAPHORE_DIR/slot-$1" # this will fail if we had not aqcuired a semaphore!
 }
 
 bats_semaphore_get_free_slot_count() {
     # find might error out without returning something useful when a file is deleted,
     # while the directory is traversed ->  only continue when there was no error
-    until used_slots=$(find "$BATS_SEMAPHORE_DIR" -name 'pid-*' 2>/dev/null | wc -l); do :; done
+    until used_slots=$(find "$BATS_SEMAPHORE_DIR" -name 'slot-*' 2>/dev/null | wc -l); do :; done
     echo $(( BATS_SEMAPHORE_NUMBER_OF_SLOTS - used_slots ))
 }
 
