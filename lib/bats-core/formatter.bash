177228e2c3ba743b5b26ecaa1a3f1e569c73e7f9
Martin Schulze <martin.schulze@vireso.de>
2022-07-26 13:27:34 +0200

    pretty: Remove duplicate timing information from test name

--- lib/bats-core/formatter.bash
+++ lib/bats-core/formatter.bash
@@ -54,7 +54,7 @@ function bats_parse_internal_extended_tap() {
                 else
                     scope=ok
                     if [[ "$line" =~ $timing_expr ]]; then
-                        bats_tap_stream_ok --duration "${BASH_REMATCH[1]}" "$ok_index" "$test_name"
+                        bats_tap_stream_ok --duration "${BASH_REMATCH[1]}" "$ok_index" "${test_name% in ${BASH_REMATCH[1]}ms}"
                     else
                         bats_tap_stream_ok "$ok_index" "$test_name"
                     fi
@@ -71,7 +71,7 @@ function bats_parse_internal_extended_tap() {
                 not_ok_index="${BASH_REMATCH[1]}"
                 test_name="${BASH_REMATCH[2]}"
                 if [[ "$line" =~ $timing_expr ]]; then
-                    bats_tap_stream_not_ok --duration "${BASH_REMATCH[1]}" "$not_ok_index" "$test_name"
+                    bats_tap_stream_not_ok --duration "${BASH_REMATCH[1]}" "$not_ok_index" "${test_name% in ${BASH_REMATCH[1]}ms}"
                 else
                     bats_tap_stream_not_ok "$not_ok_index" "$test_name"
                 fi
