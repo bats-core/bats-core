4da0c482aa69151735b8230a97b65c51b0fcb884
yashwankhede <yashwankhede003@gmail.com>
2023-04-26 20:47:38 +0530

    reduced the time complexity of shellcheck.sh
    Signed-off-by: yashwankhede <yashwankhede003@gmail.com>

--- shellcheck.sh
+++ shellcheck.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
-
 set -e
 
-targets=()
-while IFS= read -r -d $'\0'; do
-  targets+=("$REPLY")
-done < <(
-  find . -type f \( -name \*.bash -o -name \*.sh \) -print0
-  find . -type f -name '*.bats' -not -name '*_no_shellcheck*' -print0
-  find libexec -type f -print0
-  find bin -type f -print0
-)
+targets=$(find . -type f \( -name '*.bash' -o -name '*.sh' -o -name '*.bats' -not -name '*_no_shellcheck*' \) -print0)
 
 if [[ $1 == --list ]]; then
-  printf "%s\n" "${targets[@]}"
-  exit 0
+    printf "%s\n" "${targets[@]}"
+    exit 0
 fi
 
-LC_ALL=C.UTF-8 shellcheck "${targets[@]}"
-
-exit $?
+LC_ALL=C.UTF-8 shellcheck ${targets}
