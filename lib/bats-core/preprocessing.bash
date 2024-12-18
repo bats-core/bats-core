f6a01577a1d308f9f10a7ffc71b33e2c22df384d
Martin Schulze <martin.schulze@vireso.de>
2024-12-19 00:09:44 +0100

    fix gather-tests breaking under noclobber (fixes #1039, #976)
    improve naming and structure of BATS_OUT/src files in BATS_RUN_TMPDIR

--- lib/bats-core/preprocessing.bash
+++ lib/bats-core/preprocessing.bash
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
-BATS_TMPNAME="$BATS_RUN_TMPDIR/bats.$$"
-BATS_PARENT_TMPNAME="$BATS_RUN_TMPDIR/bats.$PPID"
-# shellcheck disable=SC2034
-BATS_OUT="${BATS_TMPNAME}.out" # used in bats-exec-file
-
-bats_preprocess_source() {
+bats_export_preprocess_source_BATS_TEST_SOURCE() {
   # export to make it visible to bats_evaluate_preprocessed_source
   # since the latter runs in bats-exec-test's bash while this runs in bats-exec-file's
-  export BATS_TEST_SOURCE="${BATS_TMPNAME}.src"
+  export BATS_TEST_SOURCE="$BATS_RUN_TMPDIR/${BATS_TEST_FILE_NUMBER?}-${BATS_TEST_FILENAME##*/}.src"
+}
+
+bats_preprocess_source() { # index
+  bats_export_preprocess_source_BATS_TEST_SOURCE
   # shellcheck disable=SC2153
   CHECK_BATS_COMMENT_COMMANDS=1 "$BATS_ROOT/libexec/bats-core/bats-preprocess" "$BATS_TEST_FILENAME" >"$BATS_TEST_SOURCE"
 }
 
 bats_evaluate_preprocessed_source() {
-  if [[ -z "${BATS_TEST_SOURCE:-}" ]]; then
-    BATS_TEST_SOURCE="${BATS_PARENT_TMPNAME}.src"
-  fi
   # Dynamically loaded user files provided outside of Bats.
   # shellcheck disable=SC1090
-  source "$BATS_TEST_SOURCE"
+  source "${BATS_TEST_SOURCE?}"
 }
