b7e3bf544d6450e013bb5b77901ee8a019e08f14
Martin Schulze <martin.schulze@vireso.de>
2025-01-21 22:58:31 +0100

    fix: FD3 don't wait for newline (fixes #1049)

--- lib/bats-core/validator.bash
+++ lib/bats-core/validator.bash
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 bats_test_count_validator() {
-  trap '' INT # continue forwarding
   header_pattern='[0-9]+\.\.[0-9]+'
   IFS= read -r header
-  # repeat the header
-  printf "%s\n" "$header"
-
   # if we detect a TAP plan
   if [[ "$header" =~ $header_pattern ]]; then
     # extract the number of tests ...
@@ -14,8 +10,6 @@ bats_test_count_validator() {
     # ... count the actual number of [not ] oks...
     local actual_number_of_tests=0
     while IFS= read -r line; do
-      # forward line
-      printf "%s\n" "$line"
       case "$line" in
       'ok '*)
         ((++actual_number_of_tests))
@@ -30,8 +24,5 @@ bats_test_count_validator() {
       printf '# bats warning: Executed %s instead of expected %s tests\n' "$actual_number_of_tests" "$expected_number_of_tests"
       return 1
     fi
-  else
-    # forward output unchanged
-    cat
   fi
 }
