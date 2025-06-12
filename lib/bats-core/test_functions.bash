bd1f22d798d43d5e1b65db4f3765595e8985f188
jakub-bochenski <kuba.bochenski@gmail.com>
2025-06-12 16:30:23 +0200

    fix: Unset sterr in run()
    Otherwise if you mix regular `run` with `run --separate-stderr` you could get stderr from previus command.
    
    Unset output for good measure too

--- lib/bats-core/test_functions.bash
+++ lib/bats-core/test_functions.bash
@@ -348,6 +348,8 @@ run() { # [!|-N] [--keep-empty-lines] [--separate-stderr] [--] <command to run..
     bats_warn_minimum_guaranteed_version "Using flags on \`run\`" 1.5.0
   fi
 
+  unset output stderr
+
   local pre_command=
 
   case "$output_case" in
